c882d51336193d64d6b26b22a7fc2fc85b790d2c
karanlnt <100667916+karanlnt@users.noreply.github.com>
2025-05-22 04:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world... Hope you are doing well....This is to test our CI/CD pipeline'
+    return 'Hello world... Hope you are doing well....This is to test our CI/CD pipeline1'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
